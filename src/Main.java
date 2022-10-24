166e627328ae7ec88dd458b120169424896e54f4
Christopher Leach <leach@hey.com>
2022-10-24 14:17:19 -0400

    Completed part 1 and implemented it correctly, currently need to add an iterator to Sorter.java

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,23 @@ public class Main {
         // print the array
         System.out.println("Array before sorting: " + Arrays.toString(array));
         // sort the array using a insertion sort from main
-       // Main.insertionSort(array); currently not working
+        Sorter.insertionSort(array, array.length);
+        // print the array
+        System.out.println("Array after sorting: " + Arrays.toString(array));
+        // create a second array to sort via selection sort
+        int[] array2 = createArray();
+        // print the array
+        System.out.println("Array II before sorting: " + Arrays.toString(array2));
+        // sort using selection sort
+        Sorter.selectionSort(array2, array2.length);
+        // print the array
+        System.out.println("Array II after sorting: " + Arrays.toString(array2));
 
 
     }
 
     /**
-     * Creates an array of 20 random integers
+     * Creates an array of 20 random Integer values
      * @return the array
      */
     public static int[] createArray() {
@@ -24,92 +34,4 @@ public class Main {
         }
         return array;
     }
-    /**
-     * Prints the array
-     * @param array the array to print
-     */
-    public static void printArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println();
-    }
-
-    // SELECTION SORT
-    /** Sorts the first n objects in an array into ascending order.
-     @param a  An array of Comparable objects.
-     @param n  An integer > 0. */
-    public static void selectionSort(int[] a, int n)
-    {
-        for (int index = 0; index < n - 1; index++)
-        {
-            int indexOfNextSmallest = getIndexOfSmallest(a, index, n - 1);
-            swap(a, index, indexOfNextSmallest);
-            // Assertion: a[0] <= a[1] <= . . . <= a[index] <= all other a[i]
-        } // end for
-    } // end selectionSort
-
-    // Finds the index of the smallest value in a portion of an array a.
-    // Precondition: a.length > last >= first >= 0.
-    // Returns the index of the smallest value among
-    // a[first], a[first + 1], . . . , a[last].
-    private static int getIndexOfSmallest(int[] a, int first, int last)
-    {
-        int min = a[first];
-        int indexOfMin = first;
-        for (int index = first + 1; index <= last; index++)
-        {
-            if (a[index] < 0)
-            {
-                min = a[index];
-                indexOfMin = index;
-            } // end if
-            // Assertion: min is the smallest of a[first] through a[index].
-        } // end for
-
-        return indexOfMin;
-    } // end getIndexOfSmallest
-
-// -------------------------------------------------------------------------------
-
-    // INSERTION SORT
-    public static void insertionSort(int[] a, int n)
-    {
-        insertionSort(a, 0, n - 1);
-    } // end insertionSort
-
-    public static void insertionSort(int[] a, int first, int last)
-    {
-        for (int unsorted = first + 1; unsorted <= last; unsorted++)
-        {   // Assertion: a[first] <= a[first + 1] <= ... <= a[unsorted - 1]
-
-            int firstUnsorted = a[unsorted];
-
-            insertInOrder(firstUnsorted, a, first, unsorted - 1);
-        } // end for
-    } // end insertionSort
-
-    private static void insertInOrder(int anEntry, int[] a, int begin, int end)
-    {
-        int index = end;
-
-        while ((index >= begin) && (a[index]) < 0)
-        {
-            a[index + 1] = a[index]; // Make room
-            index--;
-        } // end for
-
-        // Assertion: a[index + 1] is available
-        a[index + 1] = anEntry;  // Insert
-    } // end insertInOrder
-
-    // -------------------------------------------------------------------------------
-    // Swaps the array entries a[i] and a[j].
-    private static void swap(int[] a, int i, int j)
-    {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    } // end swap
-
 }
---
+++ src/Sorter.java
@@ -0,0 +1,89 @@
+public class Sorter {
+    // SELECTION SORT
+    /** Sorts the first n objects in an array into ascending order.
+     @param a  An array of Comparable objects.
+     @param n  An integer > 0. */
+    public static void selectionSort(int[] a, int n)
+    {
+        for (int index = 0; index < n - 1; index++)
+        {
+            int indexOfNextSmallest = getIndexOfSmallest(a, index, n - 1);
+            swap(a, index, indexOfNextSmallest);
+            printArray(a);
+            // Assertion: a[0] <= a[1] <= . . . <= a[index] <= all other a[i]
+        } // end for
+    } // end selectionSort
+
+    // Finds the index of the smallest value in a portion of an array a.
+    // Precondition: a.length > last >= first >= 0.
+    // Returns the index of the smallest value among
+    // a[first], a[first + 1], . . . , a[last].
+    private static int getIndexOfSmallest(int[] a, int first, int last)
+    {
+        int min = a[first];
+        int indexOfMin = first;
+        for (int index = first + 1; index <= last; index++)
+        {
+            if (a[index] < min)
+            {
+                min = a[index];
+                indexOfMin = index;
+            } // end if
+            // Assertion: min is the smallest of a[first] through a[index].
+        } // end for
+
+        return indexOfMin;
+    } // end getIndexOfSmallest
+
+// -------------------------------------------------------------------------------
+
+    // INSERTION SORT
+    public static void insertionSort(int[] a, int n)
+    {
+        insertionSort(a, 0, n - 1);
+    } // end insertionSort
+
+
+    public static void insertionSort(int[] a, int first, int last)
+    {
+        for (int unsorted = first + 1; unsorted <= last; unsorted++)
+        {   // Assertion: a[first] <= a[first + 1] <= ... <= a[unsorted - 1]
+
+            int firstUnsorted = a[unsorted];
+
+            insertInOrder(firstUnsorted, a, first, unsorted - 1);
+            printArray(a);
+        } // end for
+    } // end insertionSort
+
+    private static void insertInOrder(int anEntry, int[] a, int begin, int end)
+    {
+        int index = end;
+
+        while ((index >= begin) && (anEntry < a[index]))
+        {
+            a[index + 1] = a[index]; // Make room
+            index--;
+        } // end for
+
+        // Assertion: a[index + 1] is available
+        a[index + 1] = anEntry;  // Insert
+    } // end insertInOrder
+
+// -------------------------------------------------------------------------------
+
+    private static void swap(int[] a, int i, int j)
+    {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    } // end swap
+
+    //method to print the array during each step of the sort
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
